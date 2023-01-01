4bc37c4e02bde7dbd098f1ae11e1e7b5643831f1
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-01-01 17:56:56 +0300

    creating diffrent pages && routes

---
+++ pages/contactt/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <h1>this is index page</h1>
+
+      {/* Important */}
+      {/* 
+        if the name of file is : index 
+ we can go to this file without
+   index name ex//
+               path/contactt*/}
+    </div>
+  )
+}
+
+export default index
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-   <h1>set up sucessfully</h1>
+   <h1>Home page</h1>
     </div>
   )
 }
